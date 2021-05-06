23b86092b8517c0a204194d44dc7efafa6532d9f
HoangNghia <hoangnghia.binhthuan@gmail.com>
2021-05-06 20:08:25 +0700

    implement .env > connect to mongoDB >  add colors to the console

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import products from './data/products.js';
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -19,4 +26,12 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log('Server running on port 5000 ... !!!!'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT} ... !!!!`
+      .yellow.bold
+  )
+);
