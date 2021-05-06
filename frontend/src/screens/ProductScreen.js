42d04d853ecaec8b4f92c48be2077a7f432a901d
HoangNghia <hoangnghia.binhthuan@gmail.com>
2021-05-06 14:50:35 +0700

    fetch products from backend to react-fontend > add useState & useEffect

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
 
 const ProductScreen = (props) => {
-  const product = products.find((p) => p._id === props.match.params.id);
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      // const { data } = await axios.get('/api/products');
+      const { data } = await axios.get(
+        `/api/products/${props.match.params.id}`
+      );
+      setProduct(data);
+
+      //console.log(`Resolve of promise: ${JSON.stringify(res)}`);
+    };
+
+    fetchProduct();
+  }, []);
+
+  // const product = products.find((p) => p._id === props.match.params.id);
 
   return (
     <>
@@ -19,7 +36,7 @@ const ProductScreen = (props) => {
         <Col md={3}>
           <ListGroup variant='flush'>
             <ListGroup.Item>
-              <h3>{product.name}</h3>
+              <p className='font-weight-bold'>{product.name}</p>
             </ListGroup.Item>
 
             <ListGroup.Item>
