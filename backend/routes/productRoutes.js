6e6443a8706851049c963189cbb75bbabfe1fd0b
HoangNghia <hoangnghia.binhthuan@gmail.com>
2021-05-08 22:26:09 +0700

    replace library  express-async-handler to express-async-errors

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,36 +1,31 @@
 import express, { Router } from 'express';
 const router = express.Router();
 import Product from '../model/productModel.js';
-import AsyncHandler from 'express-async-handler';
+// import AsyncHandler from 'express-async-handler';
+import 'express-async-errors';
 
 // @desc: Fetch all products
 // @route: GET /api/product
 // @access: Public
-router.get(
-  '/',
-  AsyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    if (products) {
-      res.json(products);
-    } else {
-      res.status(404).json({ message: 'The product list is empty !' });
-    }
-  })
-);
+router.get('/', async (req, res) => {
+  const products = await Product.find({});
+  if (products) {
+    res.json(products);
+  } else {
+    res.status(404).json({ message: 'The product list is empty !' });
+  }
+});
 
 // @desc: Fetch single product
 // @route: GET /api/product/:id
 // @access: Public
-router.get(
-  '/:id',
-  AsyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404).json({ message: 'Product not found !' });
-    }
-  })
-);
+router.get('/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Product not found !' });
+  }
+});
 
 export default router;
