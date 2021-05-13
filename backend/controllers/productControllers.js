e4fda34240cfcf080b8191bccfb2f1fdc8e30ccc
HoangNghia <hoangnghia.binhthuan@gmail.com>
2021-05-13 17:11:40 +0700

    implement controller in backend for clean structure

---
+++ backend/controllers/productControllers.js
@@ -0,0 +1,21 @@
+import Product from '../model/productModel.js';
+// import AsyncHandler from 'express-async-handler';
+import 'express-async-errors';
+
+export const getProducts = async (req, res) => {
+  const products = await Product.find({});
+  if (products) {
+    res.json(products);
+  } else {
+    res.status(404).json({ message: 'The product list is empty !' });
+  }
+};
+
+export const getProductById = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Product not found !' });
+  }
+};
