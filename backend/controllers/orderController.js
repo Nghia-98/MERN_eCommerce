095bc7c197a3996710d96ad66e42b54cb7cfb381
HoangNghia <hoangnghia.binhthuan@gmail.com>
2021-06-15 17:06:56 +0700

    create getOrderById endpoint (/api/order/:id)

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ import 'express-async-errors';
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
-export const addOrderItems = async (req, res) => {
+const addOrderItems = async (req, res) => {
   const {
     orderItems,
     shippingAddress,
@@ -37,3 +37,21 @@ export const addOrderItems = async (req, res) => {
     res.status(201).json(createdOrder);
   }
 };
+
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = async (req, res) => {
+  const orderId = req.params.id;
+
+  const order = await Order.findById(orderId).populate('user', 'name email');
+
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+};
+
+export { addOrderItems, getOrderById };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,11 +2,16 @@ import express from 'express';
 const router = express.Router();
 import auth from '../middleware/authMiddleware.js';
 
-import { addOrderItems } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
 router.route('/').post(auth.isLogin, addOrderItems);
+
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+router.route('/:id').get(auth.isLogin, getOrderById);
 
 export default router;
