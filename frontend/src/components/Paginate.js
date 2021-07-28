960311e1505ac23ea067395bcef7094a38e45896
HoangNghia <hoangnghia.binhthuan@gmail.com>
2021-07-29 01:36:19 +0700

    create custom pagination

--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Pagination } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
@@ -8,6 +7,15 @@ const Paginate = ({
   keyword = '',
   isAdmin = false,
 }) => {
+  const isSiblingsPage = (pageNumber) => {
+    return (
+      pageNumber === currentPage - 1 ||
+      pageNumber === currentPage - 2 ||
+      pageNumber === currentPage + 1 ||
+      pageNumber === currentPage + 2
+    );
+  };
+
   const getFilterUrl = (pageNumber) => {
     const userFilterNoKeyword = `/page/${pageNumber}`;
     const userFilterWithKeyword = `/search/${keyword}/page/${pageNumber}`;
@@ -28,17 +36,58 @@ const Paginate = ({
       }
     }
   };
-  return totalPages > 1 ? (
+
+  // if (totalPages === 1) return null;
+
+  return (
     <Pagination>
+      <LinkContainer key={'first'} to={getFilterUrl(1)}>
+        <Pagination.First active={false} />
+      </LinkContainer>
+
+      <LinkContainer
+        key={'prev'}
+        to={getFilterUrl(currentPage > 1 ? currentPage - 1 : 1)}
+      >
+        <Pagination.Prev />
+      </LinkContainer>
+
       {[...Array(totalPages).keys()].map((x) => (
         <LinkContainer key={x + 1} to={getFilterUrl(x + 1)}>
-          <Pagination.Item active={x + 1 === currentPage}>
-            {x + 1}
-          </Pagination.Item>
+          {x + 1 === 1 || x + 1 === totalPages ? (
+            <Pagination.Item active={x + 1 === currentPage}>
+              {x + 1}
+            </Pagination.Item>
+          ) : isSiblingsPage(x + 1) ? (
+            <Pagination.Item active={x + 1 === currentPage}>
+              {x + 1}
+            </Pagination.Item>
+          ) : x + 1 === currentPage ? (
+            <Pagination.Item active={true}>{x + 1}</Pagination.Item>
+          ) : x + 1 === 2 ? (
+            <Pagination.Ellipsis />
+          ) : x + 1 === totalPages - 1 ? (
+            <Pagination.Ellipsis />
+          ) : (
+            <div className='d-none'></div>
+          )}
         </LinkContainer>
       ))}
+
+      <LinkContainer
+        key={'next'}
+        to={getFilterUrl(
+          currentPage < totalPages ? currentPage + 1 : totalPages
+        )}
+      >
+        <Pagination.Next />
+      </LinkContainer>
+
+      <LinkContainer key={'last'} to={getFilterUrl(totalPages)}>
+        <Pagination.Last />
+      </LinkContainer>
     </Pagination>
-  ) : null;
+  );
 };
 
 export default Paginate;
